00ad1e6e0480cc52c5a15dfe6e69e239787acbfd
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-12-19 20:54:41 +0800

    create base html for button selector and random recipiece

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [recipe, setRecipe] = useState(null);
+
+  const getRandomRecipe = async () => {
+    try {
+      const response = await axios.get(
+        "https://www.themealdb.com/api/json/v1/1/random.php"
+      );
+      setRecipe(response.data.meals[0]);
+    } catch (error) {
+      console.error("Error fetching recipe:", error);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1>Random Food Recipe Generator</h1>
+      <button onClick={getRandomRecipe}>Get Random Recipe</button>
+      {recipe && (
+        <div>
+          <h2>{recipe.strMeal}</h2>
+          <img src={recipe.strMealThumb} alt={recipe.strMeal} />
+          <p>{recipe.strInstructions}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
